59cda531ba9301252e734ac5634a4297a2315134
pcj2567 <minsu041019@gmail.com>
2025-08-27 20:49:39 +0900

    .
    add SeaCreatureTemplate

---
+++ DTDProject/Source/DTDProject/SeaCreature/Animation/SeaCreatureAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SeaCreatureAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DTDPROJECT_API USeaCreatureAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};

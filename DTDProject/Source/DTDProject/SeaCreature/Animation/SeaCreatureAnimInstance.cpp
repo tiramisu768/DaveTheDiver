dabb7352c34de351b4707982812c0b1ed2d10a79
tiramisu768 <122328175+tiramisu768@users.noreply.github.com>
2025-08-28 17:14:15 +0900

    .
    update SeaCreatureTemplate

--- DTDProject/Source/DTDProject/SeaCreature/Animation/SeaCreatureAnimInstance.cpp
+++ DTDProject/Source/DTDProject/SeaCreature/Animation/SeaCreatureAnimInstance.cpp
@@ -2,4 +2,23 @@
 
 
 #include "SeaCreature/Animation/SeaCreatureAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "SeaCreature/SeaCreature.h"
 
+void USeaCreatureAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Speed = 0.f; hasTarget = false; isInvestigating = false; isDead = false;
+}
+
+void USeaCreatureAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if (ASeaCreature* OwningCharacter = Cast<ASeaCreature>(TryGetPawnOwner()))
+	{
+		Speed = OwningCharacter->GetVelocity().Size();
+		GEngine->AddOnScreenDebugMessage(-6, 2.0f, FColor::Black, FString::Printf(TEXT("Speed: %f"), Speed));
+		//isDead = OwningCharacter->isDead();
+	}
+}
--- DTDProject/Source/DTDProject/SeaCreature/Animation/SeaCreatureAnimInstance.h
+++ DTDProject/Source/DTDProject/SeaCreature/Animation/SeaCreatureAnimInstance.h
@@ -13,5 +13,26 @@ UCLASS()
 class DTDPROJECT_API USeaCreatureAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Locomotion", meta = (AllowPrivateAccess = "true"))
+	float Speed{ 0.0f };
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Locomotion", meta = (AllowPrivateAccess = "true"))
+	bool hasTarget{ false }; // 플레이어 감지 여부
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Locomotion", meta = (AllowPrivateAccess = "true"))
+	bool isInvestigating{ false }; // 놓친 직후 물음표 띄우고 머무는 상태
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "State", meta = (AllowPrivateAccess = "true"))
+	bool isDead{ false };
+public:
+    virtual void NativeInitializeAnimation() override;
+	void NativeUpdateAnimation(float DeltaSeconds) override;
+
+    /** 외부(캐릭터/AI)에서 세팅 */
+   /* UFUNCTION(BlueprintCallable, Category = "Anim")
+    void SetHasTarget(bool b) { hasTarget = b; }
+
+    UFUNCTION(BlueprintCallable, Category = "Anim")
+    void SetInvestigating(bool b) { isInvestigating = b; }
+
+    UFUNCTION(BlueprintCallable, Category = "Anim")
+    void SetDead(bool b) { isDead = b; }*/
 };
